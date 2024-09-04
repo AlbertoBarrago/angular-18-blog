202c41d7ca447ec53052cf55f6b480b9283ce8eb
Alberto Barrago <albertobarrago@gmail.com>
2024-09-04 23:24:40 +0200

    Added dark mode

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -5,11 +5,13 @@ import { MatTooltip } from '@angular/material/tooltip';
 import { NgIf } from '@angular/common';
 import { AuthService } from '../../../services/auth.service';
 import { UtilService } from '../../../services/util.service';
+import { ThemeService } from '../../../services/theme.service';
+import { MatIconButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatIcon, MatToolbar, MatTooltip, NgIf],
+  imports: [MatIcon, MatToolbar, MatTooltip, NgIf, MatIconButton],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
@@ -17,4 +19,15 @@ export class HeaderComponent {
   loginService = inject(AuthService);
   utilService = inject(UtilService);
   title = 'SmurtApp';
+  isDarkMode!: boolean;
+  themeService = inject(ThemeService);
+
+  constructor() {
+    this.isDarkMode = this.themeService.isDarkMode();
+  }
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    this.themeService.setDarkMode(this.isDarkMode);
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
+  darkMode: 'class',
   theme: {
     colors: {
-      primary: '#2577c1',
-      'secondary-bg': '#fff',
-      theme: '#fff',
-      'header-color': '#c23fe2',
-      'route-link-active': '#fff',
-      'link-color': '#555050',
-      'border-color': '#555050',
-
-      'dark-primary': '#ff500b',
-      'dark-secondary-bg': '#424242',
-      'dark-theme': '#424242',
-      'dark-header-color': '#424242',
-      'dark-route-link-active': '#ff500b',
-      'dark-link-color': '#fff',
-      'dark-border-color': '#1cd61c',
+      'dark-background': '#0a0a0f',
+      'dark-primary': '#15151e',
+      'dark-secondary': '#1f1f2c',
+      'dark-text': '#f1f1f1',
+      'dark-text-secondary': '#b4b4b4',
+      'dark-text-tertiary': '#838383',
+      'dark-text-quaternary': '#646464',
+      'dark-neon-cyan': '#00ffff',
+      'dark-neon-magenta': '#ff00ff',
+      'dark-neon-yellow': '#ffff00',
+      'dark-neon-green': '#39ff14',
+      'dark-electric-blue': '#0892d0',
+      'dark-cyber-purple': '#8a2be2',
     },
-    extend: {},
+    extend: {
+      boxShadow: {
+        'neon-glow':
+          '0 0 5px theme("colors.neon-cyan"), 0 0 20px theme("colors.neon-cyan")',
+      },
+      textShadow: {
+        cyber:
+          '0 0 5px theme("colors.neon-magenta"), 0 0 10px theme("colors.neon-magenta")',
+      },
+    },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 };
