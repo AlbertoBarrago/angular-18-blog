7231806d023ad969bf14aa9c731781b37c77d1f1
Alberto Barrago <albertobarrago@gmail.com>
2024-09-25 15:35:41 +0200

    Fixes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,4 @@ export const environment = {
   version: '0.1.0',
   apiUrl: 'https://angular-18-blog-server.onrender.com',
   production: true,
-  mongoose_connection:
-    'mongodb+srv://albertobarrago:ABVzQfbI1788bx9U@albz-cluster.2s4qrli.mongodb.net/',
 };
